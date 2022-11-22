1ba0a16500fca30d524a6af25fcc7839f2e78e68
yrsolanataonmi <yrsolanaratonmi@gmail.com>
2022-11-23 00:50:31 +0300

    передача данных в компонент через @Input

--- src/app/all-heroes/all-heroes.component.ts
+++ src/app/all-heroes/all-heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {IHero} from "../hero/hero.interface";
+import {data} from "./heroes.data";
 
 @Component({
   selector: 'app-all-heroes',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-heroes.component.css']
 })
 export class AllHeroesComponent implements OnInit {
-
+  heroes : Array<IHero> = data
   constructor() { }
 
   ngOnInit(): void {
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {IHero} from "./hero.interface";
 
 @Component({
   selector: 'app-hero',
@@ -11,5 +12,5 @@ export class HeroComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  @Input() hero: IHero
 }
